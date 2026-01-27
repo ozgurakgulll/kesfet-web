7a29eca788b73d7d90b9360eaa1fe4e67e0df08d
ozgurakgulll <89040429+ozgurakgulll@users.noreply.github.com>
2026-01-27 13:26:14 +0300

    View

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: "modern-compiler",
-        },
-      },
-    },
-  },
+  output: "static",   // 🔑 BU SATIR
   integrations: [react(), tailwind()],
 });
